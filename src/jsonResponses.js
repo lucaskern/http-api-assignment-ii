9fac152ffad16ee679d4f5c980ab53d2456d6462
Lucas Russell Kern <lucaskern8@gmail.com>
2018-02-09 13:42:17 -0500

    pages now dont redirect, adding users with date works but not with name/age

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -9,104 +9,129 @@ const users = {};
 // function to respond with a json object
 // takes request, response, status code and object to send
 const respondJSON = (request, response, status, object) => {
-  // object for our headers
-  // Content-Type for json
-  const headers = {
-    'Content-Type': 'application/json',
-  };
+    // object for our headers
+    // Content-Type for json
+    const headers = {
+        'Content-Type': 'application/json',
+    };
 
     // send response with json object
-  response.writeHead(status, headers);
-  response.write(JSON.stringify(object));
-  response.end();
+    response.writeHead(status, headers);
+    response.write(JSON.stringify(object));
+    response.end();
+
+    //console.dir(response);
 };
 
 // function to respond without json body
 // takes request, response and status code
 const respondJSONMeta = (request, response, status) => {
-  // object for our headers
-  // Content-Type for json
-  const headers = {
-    'Content-Type': 'application/json',
-  };
+    // object for our headers
+    // Content-Type for json
+    const headers = {
+        'Content-Type': 'application/json',
+    };
 
     // send response without json object, just headers
-  response.writeHead(status, headers);
-  response.end();
+    response.writeHead(status, headers);
+    response.end();
 };
 
 // get user object
 // should calculate a 200
 const getUsers = (request, response) => {
-  // json object to send
-  const responseJSON = {
-    users,
-  };
+    // json object to send
+    
+    console.dir(users);
+    const responseJSON = {
+        users,
+    };
 
     // return 200 with message
-  return respondJSON(request, response, 200, responseJSON);
+    return respondJSON(request, response, 200, responseJSON);
 };
 
 // get meta info about user object
 // should calculate a 200
 const getUsersMeta = (request, response) =>
-  // return 200 without message, just the meta data
-  respondJSONMeta(request, response, 200);
+    // return 200 without message, just the meta data
+    respondJSONMeta(request, response, 200);
 
 // function just to update our object
 const updateUser = (request, response) => {
-  let body = '';
-  let post = '';
+    let body = [];
+    request.on('data', (chunk) => {
+        body.push(chunk);
+    }).on('end', () => {
+        body = Buffer.concat(body).toString();
+        // at this point, `body` has the entire request body stored in it as a string
+        console.dir(body);
 
-  request.on('data', (data) => {
-    body += data;
-  });
+//        // change to make to user
+//        const newUser = {
+//            age: body.age,
+//            name: body.name,
+//        };
+        
+        const newUser = {
+            createdAt: Date.now(),
+            age: body.age,
+            name: body.name,
+          };
 
-  request.on('end', () => {
-    post = qs.parse(body);
-    // use post['blah'], etc.
-    console.dir(post);
+        // modifying our dummy object
+        // just indexing by time for now
+        //users[newUser.name] = newUser;
+            
+        users[newUser.createdAt] = newUser;
 
-    // change to make to user
-    const newUser = {
-      age: post.age,
-      name: post.name,
-    };
+        // console.dir(request.body);
 
-    // modifying our dummy object
-    // just indexing by time for now
-    users[newUser.name] = newUser;
+        // return a 201 created status
+        return respondJSON(request, response, 201, newUser);
+    });
 
-    // console.dir(request.body);
-
-    // return a 201 created status
-    return respondJSON(request, response, 201, newUser);
-  });
 };
+
+////function just to update our object 
+//const updateUser = (request, response) => {
+//  //change to make to user
+//  //This is just a dummy object for example
+//  const newUser = {
+//    createdAt: Date.now(),
+//  };
+//
+//  // modifying our dummy object
+//  // just indexing by time for now
+//  users[newUser.createdAt] = newUser;
+//
+//  //return a 201 created status
+//  return respondJSON(request, response, 201, newUser);
+//};
 
 // function for 404 not found requests with message
 const notFound = (request, response) => {
-  // create error message for response
-  const responseJSON = {
-    message: 'The page you are looking for was not found.',
-    id: 'notFound',
-  };
+    // create error message for response
+    const responseJSON = {
+        message: 'The page you are looking for was not found.',
+        id: 'notFound',
+    };
 
     // return a 404 with an error message
-  respondJSON(request, response, 404, responseJSON);
+    respondJSON(request, response, 404, responseJSON);
 };
 
 // function for 404 not found without message
 const notFoundMeta = (request, response) => {
-  // return a 404 without an error message
-  respondJSONMeta(request, response, 404);
+    // return a 404 without an error message
+    respondJSONMeta(request, response, 404);
 };
 
 // set public modules
 module.exports = {
-  getUsers,
-  getUsersMeta,
-  updateUser,
-  notFound,
-  notFoundMeta,
+    getUsers,
+    getUsersMeta,
+    updateUser,
+    notFound,
+    notFoundMeta,
 };
--- src/server.js
+++ src/server.js
@@ -7,49 +7,48 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 // function to handle requests
 const onRequest = (request, response) => {
-  // parse url into individual parts
-  // returns an object of url parts by name
-  const parsedUrl = url.parse(request.url);
+    // parse url into individual parts
+    // returns an object of url parts by name
+    const parsedUrl = url.parse(request.url);
 
-  // check the request method (get, head, post, etc)
-  switch (request.method) {
-    case 'GET':
-      if (parsedUrl.pathname === '/') {
-        // if homepage, send index
-        htmlHandler.getIndex(request, response);
-      } else if (parsedUrl.pathname === '/style.css') {
-        // if stylesheet, send stylesheet
-        htmlHandler.getCSS(request, response);
-      } else if (parsedUrl.pathname === '/getUsers') {
-        // if get users, send user object back
-        jsonHandler.getUsers(request, response);
-      } else if (parsedUrl.pathname === '/updateUser') {
-        // if update user, change our user object
-        jsonHandler.updateUser(request, response);
-      } else {
-        // if not found, send 404 message
-        jsonHandler.notFound(request, response);
-      }
-      break;
-    case 'HEAD':
-      if (parsedUrl.pathname === '/getUsers') {
-        // if get users, send meta data back
-        jsonHandler.getUsersMeta(request, response);
-      } else {
-        // if not found send 404 without body
-        jsonHandler.notFoundMeta(request, response);
-      }
-      break;
-    case 'POST':
-      if (parsedUrl.pathname === '/addUser') {
-        // if update user, change our user object
-        jsonHandler.updateUser(request, response);
-      }
-      break;
-    default:
-      // send 404 in any other case
-      jsonHandler.notFound(request, response);
-  }
+    console.log(request.method);
+
+    // check the request method (get, head, post, etc)
+    switch (request.method) {
+        case 'GET':
+            if (parsedUrl.pathname === '/') {
+                // if homepage, send index
+                htmlHandler.getIndex(request, response);
+            } else if (parsedUrl.pathname === '/style.css') {
+                // if stylesheet, send stylesheet
+                htmlHandler.getCSS(request, response);
+            } else if (parsedUrl.pathname === '/getUsers') {
+                // if get users, send user object back
+                jsonHandler.getUsers(request, response);
+            } else if (parsedUrl.pathname === '/updateUser') {
+                // if update user, change our user object
+                jsonHandler.updateUser(request, response);
+            } else {
+                // if not found, send 404 message
+                jsonHandler.notFound(request, response);
+            }
+            break;
+        case 'HEAD':
+            if (parsedUrl.pathname === '/getUsers') {
+                // if get users, send meta data back
+                jsonHandler.getUsersMeta(request, response);
+            } else {
+                // if not found send 404 without body
+                jsonHandler.notFoundMeta(request, response);
+            }
+            break;
+        case 'POST':
+            jsonHandler.updateUser(request, response);
+            break;
+        default:
+            // send 404 in any other case
+            jsonHandler.notFound(request, response);
+    }
 };
 
 // start server
